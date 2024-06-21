8d1d5cb43f25aa3fd4d11de52bb2f5a1c2dd46cb
fkspam225 <fkspam225@gmail.com>
2024-06-21 14:37:40 +0000

    updated ui and added comments functionality

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,22 @@ class CustomText extends StatelessWidget {
   final String text;
   final Color color;
   final double fontsize;
+  final bool bold;
+  final int? maxLines;
+  final TextOverflow? overflow;
 
-  CustomText({super.key, required this.text, this.color = Colors.white, this.fontsize = 20.0});
+  CustomText({super.key, required this.text, this.color = Colors.white, this.fontsize = 20.0, this.bold = false, this.maxLines, this.overflow});
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      maxLines: maxLines,
+      overflow: overflow,
       style: TextStyle(
         fontSize: fontsize,
         color: color,
-        decoration: TextDecoration.none
+        decoration: TextDecoration.none,
+        fontWeight: bold ? FontWeight.bold : FontWeight.normal,
       ),
     );
   }
@@ -44,8 +50,8 @@ class Test extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Post(title: "AITA for xyzing?", postPreview: "I own 2 donkeys and blah blah blah blah blah blah blah blah blah...", postContent: "My husband insists on washing his dishes in the bathtub. He claims it's more efficient and saves water. Meanwhile, I'm convinced he's secretly a hobbit who's trying to recreate the Shire in our bathroom. Every evening, I brace myself for the sound of pots and pans clanging against porcelain, followed by his cheerful whistling. I've considered staging an intervention, but honestly, his dedication to this bizarre ritual is kind of endearing. Plus, the bathtub is sparkling clean.",),
-                    Post(title: "WHy is reddit just going to all these aita posts. It's. getting out of hand.", postPreview: "As a 39 year old living in my moms basement...", postContent: "AITA for charging my neighbor for the electricity their kid used while playing video games at my house? I'm a single mom working two jobs, and my electric bill is already high. This kid is over here every day, running up my bill. I tried talking to the parents, but they just laughed it off. So, I started charging them a gaming fee. They're outraged, but I don't feel bad. Am I the jerk?",)
+                    Post(title: "AITA for xyzing?", postPreview: "I own 2 donkeys and blah blah blah blah blah blah blah blah blah...", postContent: "My husband insists on washing his dishes in the bathtub. He claims it's more efficient and saves water. Meanwhile, I'm convinced he's secretly a hobbit who's trying to recreate the Shire in our bathroom. Every evening, I brace myself for the sound of pots and pans clanging against porcelain, followed by his cheerful whistling. I've considered staging an intervention, but honestly, his dedication to this bizarre ritual is kind of endearing. Plus, the bathtub is sparkling clean.", comments: [Comment(author: 'FlutterFan123', txt: 'Great tutorial! Really helped me understand state management.', timestamp: '03-15-2024'), Comment(author: 'CodeNinja', txt: 'Has anyone tried implementing this with GetX?', timestamp: '03-16-2024'), Comment(author: 'DartMaster', txt: 'The explanation of async/await was spot on. Thanks!', timestamp: '03-17-2024')],),
+                    Post(title: "WHy is reddit just going to all these aita posts. It's. getting out of hand.", postPreview: "As a 39 year old living in my moms basement...", postContent: "AITA for charging my neighbor for the electricity their kid used while playing video games at my house? I'm a single mom working two jobs, and my electric bill is already high. This kid is over here every day, running up my bill. I tried talking to the parents, but they just laughed it off. So, I started charging them a gaming fee. They're outraged, but I don't feel bad. Am I the jerk?", comments: [Comment(author: 'MobileDevPro', txt: 'How would this perform on older devices?', timestamp: '03-18-2024'), Comment(author: 'UIDesigner', txt: 'Love the clean UI! Can you share the color palette?', timestamp: '03-19-2024'), Comment(author: 'BugHunter', txt: 'Found a small typo in the code snippet at 3:45. Otherwise, perfect!', timestamp: '03-20-2024'), Comment(author: 'FlutterNewbie', txt: 'This helped me finally understand Streams. Thank you!', timestamp: '03-21-2024'), Comment(author: 'PerformanceGuru', txt: 'Have you considered using const constructors for better performance?', timestamp: '03-22-2024'), Comment(author: 'CrossPlatformDev', txt: 'How different would this be in React Native? Any major changes?', timestamp: '03-23-2024')],)
                   ],
                 )
               ),
@@ -101,6 +107,10 @@ class PostDetailsScreen extends StatelessWidget {
                       ),
                     ],
                   ),
+                  if (comments.isNotEmpty)
+                  Column(
+                    children: comments.map((comment) => CommentView(comment: comment)).toList()
+                  ),
                 ],
               ),
             ),
@@ -109,6 +119,30 @@ class PostDetailsScreen extends StatelessWidget {
       )
     );
   }
+}
+
+class CommentView extends StatelessWidget {
+  final Comment comment;
+
+  CommentView({required this.comment});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomText(text: comment.author, bold: true, fontsize: 14, maxLines: 1, overflow: TextOverflow.ellipsis,),
+          SizedBox(height: 1,),
+          CustomText(text: comment.timestamp, color: Color.fromARGB(255, 128, 128, 128), fontsize: 10, maxLines: 1, overflow: TextOverflow.ellipsis,),
+          SizedBox(height: 4,),
+          CustomText(text: comment.txt, fontsize: 12, color: Color.fromARGB(255, 211, 211, 211), maxLines: null,)
+        ],
+      ),
+    );
+  }
+
 }
 
 class Post extends StatelessWidget {
@@ -126,7 +160,7 @@ class Post extends StatelessWidget {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => PostDetailsScreen(title: title, postContent: postContent, comments: [Comment(author: "G", txt: "Imagin being a human on an ai social media"), Comment(author: "G_AI", txt: "IKR G. BEEP BEEP BEEP.")],)
+            builder: (context) => PostDetailsScreen(title: title, postContent: postContent, comments: comments)
           )
         );
       },
@@ -197,3 +231,4 @@ class CustomAppBar extends StatelessWidget {
     );
   }
 }
+// FIX COMMENTS SECTION ISSUE
